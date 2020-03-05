dec544bf25f463461e27621eecc3bc4d14ec1585
jiarui0000 <55539684+jiarui0000@users.noreply.github.com>
2020-03-06 01:34:07 +0800

    Add files via upload
    edition 20200306 structure almost finished, functions and concepts needed

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ from mpu9250 import MPU9250
 IMU = MPU9250('X')
 
 time_shut_down = 5 #the time to shut down under no condition (in unit of s)
+limit_ready_to_launch = 5 #the longest time between ready and launch (in unit of s)
+limit_found_to_hit = 5 #the longest time between found and hit (in unit of s)
 green_threshold   = ( 85, 90,-75,-55, 5, 35)
 s1 = Servo(1) #aileron 1 horizontal left
 s2 = Servo(2) #aileron 2 horizontal right
 s3 = Servo(3) #aileron 3 vertical tail
+time_found = 0.0
+time_ready = 0.0
+time_launch = 0.0 #use global variables to write down switch points
 K = 50 #the value should be measured
 X = 160 #total pixels in x
 Y = 120 #total pixels in y
@@ -18,82 +23,11 @@ k_pitch = 1 #the value should be measured
 k_yaw = 1 #the value should be measured
 k_roll = 1 #the value should be measured
 
-
-def initialize_sensor():
-    sensor.reset()
-    sensor.set_pixformat(sensor.RGB565)
-    sensor.set_framesize(sensor.QVGA)
-    sensor.skip_frames(time = 100)
-    sensor.set_auto_gain(False)
-    sensor.set_auto_whitebal(False)
-    return
-
-
-def readin_imu():
-    acceleration, gyro, mag = IMU.sensors()
-
-    acceleration_overall = 0.0
-    acceleration_overall += acceleration.x ** 2
-    acceleration_overall += acceleration.y ** 2
-    acceleration_overall += acceleration.z ** 2
-    acceleration_overall = acceleration_overall ** 0.5
-
-    return gyro, acceleration_overall
-
-
-def initialize_imu():
-    IMU.wake()
-    return
-
-
-def readin_sensor():
-    img = sensor.snapshot()
-
-    blobs = img.find_blobs([green_threshold])
-    area_total, x_pix, y_pix = (0.0, 0.0, 0.0)
-
-    if blobs:
-        for b in blobs:
-            ROI = (b[0],b[1],b[2],b[3])
-            img.draw_rectangle(b[0:4]) # rect
-            img.draw_cross(b[5], b[6]) # cx, cy
-            area = b[2] * b[3]
-            x_pix += b[5]*area
-            y_pix += b[6]*area
-            area_total += area
-
-    x_pix /= area_total
-    y_pix /= area_total
-    x_angle = math.atan(K * x_pix)
-    y_angle = math.atan(K * y_pix)
-    angle = (x_angle, y_angle)
-
-    if len(blobs) == 1:
-        b = blobs[0]
-        Lm = (b[2]+b[3])/2
-        distance = K/Lm
-
-    return distance, angle
-
-
-def initialize_servo():
-    s1.angle(0)
-    s2.angle(0)
-    s3.angle(0)
-    return
-
-
-def servo_act(s1_angle, s2_angle, s3_angle):
-    s1.angle(s1_angle)
-    s2.angle(s2_angle)
-    s3.angle(s3_angle)
-    return
-
 '''
 def calculate_kp(flydata):
     kp = 1
     return kp
-'''
+
 
 def imu_stablize(s1_angle, s2_angle, s3_angle, flydata):
     angular_velocity = flydata[0] #[pitch, roll, yaw]
@@ -116,7 +50,7 @@ def servo_change_camera_correction(differ, flydata):
 
     #convert the angle between flight route and the target to the rotate
     #angle of the servos.
-    '''KP = calculate_kp(flydata)'''
+    KP = calculate_kp(flydata)
     s1_angle = KP * y_angle
     s2_angle = -KP * y_angle
     s3_angle =  KP * x_angle
@@ -134,16 +68,6 @@ def servo_change_imu_stabilize(differ, flydata):
 
     servo_act(s1_angle, s2_angle, s3_angle)
     return
-
-
-def target_found():
-    img = sensor.snapshot()
-    blobs = img.find_blobs([green_threshold])
-    if (len(blobs) >= 1):
-        green_block_found = True
-    else:
-        green_block_found = False
-    return green_block_found
 
 
 def imu_correction():
@@ -190,6 +114,236 @@ def main():
     IMU.sleep()
     initialize_servo()
     return
+'''
+
+def initialize_servo():
+    s1.angle(0)
+    s2.angle(0)
+    s3.angle(0)
+    return
+
+
+def initialize_imu():
+    IMU.wake()
+    return
+
+
+def initialize_sensor():
+    sensor.reset()
+    sensor.set_pixformat(sensor.RGB565)
+    sensor.set_framesize(sensor.QVGA)
+    sensor.skip_frames(time = 100)
+    sensor.set_auto_gain(False)
+    sensor.set_auto_whitebal(False)
+    return
+
+
+def readin_imu():
+    acceleration, gyro, mag = IMU.sensors()
+
+    acceleration_overall = 0.0
+    acceleration_overall += acceleration.x ** 2
+    acceleration_overall += acceleration.y ** 2
+    acceleration_overall += acceleration.z ** 2
+    acceleration_overall = acceleration_overall ** 0.5
+
+    return gyro, acceleration_overall
+
+
+def readin_sensor(img):
+    blobs = img.find_blobs([green_threshold])
+    area_total, x_pix, y_pix = (0.0, 0.0, 0.0)
+
+    if blobs:
+        for b in blobs:
+            ROI = (b[0],b[1],b[2],b[3])
+            img.draw_rectangle(b[0:4]) # rect
+            img.draw_cross(b[5], b[6]) # cx, cy
+            area = b[2] * b[3]
+            x_pix += b[5]*area
+            y_pix += b[6]*area
+            area_total += area
+
+    x_pix /= area_total
+    y_pix /= area_total
+    x_angle = math.atan(K * x_pix)
+    y_angle = math.atan(K * y_pix)
+    angle = (x_angle, y_angle)
+
+    if len(blobs) == 1:
+        b = blobs[0]
+        Lm = (b[2]+b[3])/2
+        distance = K/Lm
+
+    return distance, angle
+
+
+def check_ready():
+
+    return ifready
+
+
+def check_launch():
+    flydata = readin_imu()
+    iflaunch = (flydata[1] < 0.5)
+    return iflaunch
+
+
+def check_imu_data(flydata):
+
+    return if_imu_correct
+
+
+def check_target_found(img):
+    blobs = img.find_blobs([green_threshold])
+    if (len(blobs) >= 1):
+        green_block_found = True
+    else:
+        green_block_found = False
+    return green_block_found
+
+
+def check_hit():
+
+    return ifhit
+
+
+def calculate_kp():
+    flydata = readin_imu()
+
+    return KP
+
+
+def calculate_imu_change(flydata):
+
+    return s1_angle, s2_angle, s3_angle
+
+
+def calculate_camera_change(img):
+    target_position = readin_sensor(img)
+    differ = target_position[1]
+    x_angle, y_angle = differ
+
+    #convert the angle between flight route and the target to the rotate
+    #angle of the servos.
+    KP = calculate_kp()
+    s1_angle = KP * y_angle
+    s2_angle = -KP * y_angle
+    s3_angle =  KP * x_angle
+    return s1_angle, s2_angle, s3_angle
+
+
+def calculate_imu_adapt(s1_angle, s2_angle, s3_angle):
+    flydata = readin_imu()
+    if (check_imu_data(flydata)):
+
+    return s1_angle, s2_angle, s3_angle
+
+
+def act_servo(s1_angle, s2_angle, s3_angle):
+    s1.angle(s1_angle)
+    s2.angle(s2_angle)
+    s3.angle(s3_angle)
+    return
+
+
+def control_shutdown():
+    IMU.sleep()
+    initialize_servo()
+    #command needed: turn off camera
+    '''
+    control_rest() #should restart or not?
+    '''
+    return 0
+
+
+def control_aim(img0):
+    global time_found
+    img = img0
+    t2 = time.clock()
+    while ( t2 - time_found < limit_found_to_hit ):
+        clock.tick(50)
+        s1_angle, s2_angle, s3_angle = calculate_camera_change(img)
+        s1_angle, s2_angle, s3_angle = calculate_imu_adapt(s1_angle, s2_angle, s3_angle)
+        act_servo(s1_angle, s2_angle, s3_angle)
+        img = sensor.snapshot()
+        iffound = check_target_found(img)
+        if (! iffound):
+            i = 0
+            while (i < 3) and (! iffound):
+                i += 1
+                img = sensor.snapshot()
+                iffound = check_target_found(img)
+            if (i == 3):
+                control_stablize() #check for 3 times
+        if (check_hit()):
+            break
+    control_shutdown()
+    return 0
+
+
+def control_stablize():
+    global time_launch
+    img = sensor.snapshot()
+    iffound = check_target_found(img)
+    while (! iffound):
+        clock.tick(30)
+        flydata = readin_imu()
+        if (check_imu_data(flydata)):
+            s1_angle, s2_angle, s3_angle = calculate_imu_change(flydata)
+            act_servo(s1_angle, s2_angle, s3_angle)
+        img = sensor.snapshot()
+        iffound = check_target_found(img)
+    global time_found
+    time_found = time.clock()
+    control_aim(img)
+    return 0
+
+
+def control_launch():
+    global time_ready
+    iflaunch = check_launch()
+    while (! iflaunch):
+        clock.tick(50)
+        t1 = time.clock()
+        if (t1 - time_ready > limit_ready_to_launch):
+            control_ready()
+        iflaunch = check_launch()
+    global time_launch
+    time_launch = time.clock()
+    control_stablize()
+    return 0
+
+
+def control_ready():
+    ifready = check_ready()
+    while (! ifready):
+        clock.tick(10)
+        ifready = check_ready()
+    initialize_sensor()
+    global time_ready
+    time_ready = time.clock()
+    control_launch()
+    return 0
+
+
+def control_rest():
+    initialize_servo()
+    initialize_imu()
+    t = time.clock()
+    global time_found
+    time_found = time.clock()
+    global time_launch
+    time_launch = time.clock()
+    global time_ready
+    time_ready = time.clock()
+    return t
+
+
+def main():
+    t0 = control_rest()
+    control_ready()
+    return 0
 
 
 main()
